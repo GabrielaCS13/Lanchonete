ebe1da64c6e05769a5f5ce74f0e8bf92adf338b5
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2023-05-03 16:14:48 -0300

    Adicionando o arquivo lanchonete.cs, desenvolvido em sala na aula de C#

---
+++ Lanchonete/Views/Shared/carrossel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lanchonete.Views.Shared
+{
+    public class carrosselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
